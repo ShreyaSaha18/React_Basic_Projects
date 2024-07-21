9d5502538d2f4b00bd93cdd94a1fbe0ba97eca74
Shreya Saha <shreya20211@gmail.com>
2024-07-21 17:13:48 +0530

    This is my Brand page first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Hero from "./components/Hero";
+import Nav from "./components/Nav";
+import "./App.css"
+
+const App = () => {
+  return(
+    <div>
+     <Nav></Nav>
+     <Hero></Hero>
+    </div>
+  )
+}
+export default App;
